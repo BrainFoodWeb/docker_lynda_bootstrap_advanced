e6f81773e8993d591c85adb950b7559be05814b3
Buddha1115 <barrygestwicki@gmail.com>
2014-12-06 00:31:12 -0600

    Inital commit

---
+++ _/components/php/header.php
@@ -0,0 +1,19 @@
+<div class="content row">
+	<div class="col-lg-12">
+		<header class="clearfix">
+			<section id="branding">
+				<a href="index.php"><img src="images/misc/ralogo_monogram.png" alt="Logo for Roux Conference"></a>
+			</section>
+			<section class="navbar navbar">
+				<ul class="nav navbar-nav">
+					<li><a href="index.php">Home</a></li>
+					<li><a href="venuetravel.php">Venue/Travel</a></li>
+					<li><a href="schedule.php">Schedule</a></li>
+					<li><a href="artists.php">Artists</a></li>
+					<li><a href="register.php">Register</a></li>
+				</ul>
+			</section>
+
+		</header>
+	</div>
+</div>
